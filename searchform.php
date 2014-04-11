c79e2e7fefe26ba0830b21b9ac705d6fe27f57ac
Marco <macrozone@gmail.com>
2014-04-11 15:56:39 +0200

    renamed

--- searchform.php
+++ searchform.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The template for displaying search forms in _tk
+ * The template for displaying search forms in gisu
  *
- * @package _tk
+ * @package gisu
  */
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_tk' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', '_tk' ); ?>">
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'gisu' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'gisu' ); ?>">
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', '_tk' ); ?>">
+	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'gisu' ); ?>">
 </form>
