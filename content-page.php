d2cedfb4567e7c4f4884145d852bd9755d4a371a
Marco <macrozone@gmail.com>
2014-05-23 16:56:32 +0200

    some fixes

--- content-page.php
+++ content-page.php
@@ -13,6 +13,7 @@ $templates = array(
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div id="content">
+<div id="content-wrapper">
 	
 		<?php the_content(); ?>
 		<?php
@@ -22,6 +23,7 @@ $templates = array(
 			) );
 		?>
 
+	</div>
 	</div>
 	<!--<?php edit_post_link( __( 'Edit', 'gisu' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>-->
 	
